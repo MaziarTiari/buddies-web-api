411c555408beadca857dde44dacd41528444549d
Maziar Bakhtyaryan <inf3446@hs-worms.de>
2020-05-27 04:54:27 +0200

    fix: Class not found in startup.cs

---
+++ buddiesApi/Services/UserProfileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using buddiesApi.Models;
+
+namespace buddiesApi.Services
+{
+    public class UserProfileService : Service<UserProfile>
+    {
+        public UserProfileService(IBuddiesDbContext settings) : base(settings)
+        {
+            this.collection = base.GetDatabase.GetCollection<UserProfile>(
+                settings.UserProfilesCollectionName);
+        }
+    }
+}
